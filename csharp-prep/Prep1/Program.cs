d848c24d6a8e97d3c8d7385858b72305af486bf4
Moroni-MB <moronimontoya04@gmail.com>
2025-09-20 15:51:41 -0600

    Added Prep 1 and 2

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,52 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Console.WriteLine("Hello Prep1 World!");
+        
+        // string firstName = "Bob";
+
+        // Console.WriteLine($"Hello {firstName}.");
+
+        // Console.Write("What is your favorite color? ");
+        // string favoriteColor = Console.ReadLine();
+
+        // Console.WriteLine($"{firstName}Your favorite color is {favoriteColor}");
+
+        // Console.Write("How old are you? ");
+        // int age = int.Parse(Console.ReadLine());
+
+        // Console.WriteLine($"{firstName} you are {age} years old.");
+
+        // int a = 13;
+        // int b = 7;
+
+        // int c = a + b;
+
+
+
+        // double x = 1.5;
+        // float y = 1.5f;
+
+        // bool isDone = false;
+        // if (isDone) 
+        // {
+        //     Console.WriteLine("All done.");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("Not done yet.");
+        // }
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
+
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        int GradePercentage = int.Parse(Console.ReadLine());
+        string letter = "";
+        if (GradePercentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (GradePercentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (GradePercentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (GradePercentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"Your letter grade is {letter}");
+        if (GradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations for passing the class!");
+        } else {
+            Console.WriteLine("I'm sorry, you didn't pass the class. Reach out to Academic Advising for more help!");
+        }
     }
 }
